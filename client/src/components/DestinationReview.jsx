a1c6588ee073ba545a42a8c52aaacfc2f95241e8
SAMANTHA PARKES <slmillward@outlook.com>
2025-10-17 13:12:04 +0100

    reviews displayed

--- client/src/components/DestinationReview.jsx
+++ client/src/components/DestinationReview.jsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 export function DestinationReview() {
+  const [reviews, setReviews] = useState([]);
+
+  const params = useParams();
+  const destinationId = params.id;
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `http://localhost:8080/destination/${destinationId}`
+      );
+      const data = await response.json();
+      {
+        console.log(`Here is my: ${data}`);
+      }
+      setReviews(data.reviews);
+    }
+    fetchData();
+  }, [destinationId]);
+
   return (
     <div>
-      <p>Destination review area with name and comment</p>
+      <h2>Reviews:</h2>
+      {reviews.map((review) => (
+        <div key={review.id}>
+          <p>Name: {review.name}</p>
+          <p>Comment: {review.comment}</p>
+        </div>
+      ))}
     </div>
   );
 }
